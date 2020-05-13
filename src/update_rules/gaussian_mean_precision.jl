713158e55c1208e86ed738e3a90096177eaa99e9
I. Senoz <i.senoz@tue.nl>
2020-05-13 19:15:52 +0200

    remove gcv node

--- src/update_rules/gaussian_mean_precision.jl
+++ src/update_rules/gaussian_mean_precision.jl
@@ -51,22 +51,3 @@
 @marginalRule(:node_type => GaussianMeanPrecision,
               :inbound_types => (Message{Gaussian}, Message{Gaussian}, ProbabilityDistribution),
               :name => MGaussianMeanPrecisionGGD)
-
-
-@structuredVariationalRule(:node_type => GaussianMeanPrecision,
-                           :outbound_type => Message{GaussianMeanVariance},
-                           :inbound_types => (Nothing,Message{ExponentialLinearQuadratic},ProbabilityDistribution),
-                           :name => SVBGaussianMeanPrecisionOutNED)
-
-@structuredVariationalRule(:node_type => GaussianMeanPrecision,
-                          :outbound_type => Message{GaussianMeanVariance},
-                          :inbound_types => (Message{ExponentialLinearQuadratic},Nothing,ProbabilityDistribution),
-                          :name => SVBGaussianMeanPrecisionMEND)
-
-@marginalRule(:node_type => GaussianMeanPrecision,
-            :inbound_types => (Message{Gaussian}, Message{ExponentialLinearQuadratic}, ProbabilityDistribution),
-            :name => MGaussianMeanPrecisionGED)
-
-@marginalRule(:node_type => GaussianMeanPrecision,
-            :inbound_types => (Message{ExponentialLinearQuadratic}, Message{Gaussian}, ProbabilityDistribution),
-            :name => MGaussianMeanPrecisionEGD)
