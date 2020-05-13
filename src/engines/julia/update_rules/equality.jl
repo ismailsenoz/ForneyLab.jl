634ade87e1876cf68c2bfd840fb6ca055189e9a9
I. Senoz <i.senoz@tue.nl>
2020-05-13 19:18:21 +0200

    cleanup

--- src/engines/julia/update_rules/equality.jl
+++ src/engines/julia/update_rules/equality.jl
@@ -7,8 +7,7 @@ ruleSPEqualityCategorical,
 ruleSPEqualityDirichlet,
 ruleSPEqualityPointMass,
 ruleSPEqualityRGMP,
-ruleSPEqualityGaussianRGMP,
-ruleSPEqualityGaussianGCV
+ruleSPEqualityGaussianRGMP
 
 
 ruleSPEqualityGaussian(msg_1::Message{F1}, msg_2::Message{F2}, msg_3::Nothing) where {F1<:Gaussian, F2<:Gaussian} = Message(prod!(msg_1.dist, msg_2.dist))
@@ -49,14 +48,3 @@ ruleSPEqualityGaussianRGMP(msg_1::Nothing, msg_2::Message{F1}, msg_3::Message{F2
 ruleSPEqualityGaussianRGMP(msg_1::Message{F1}, msg_2::Message{F2}, msg_3::Nothing) where {F1<:Gaussian, F2<:Function} = Message(prod!(msg_1.dist, msg_2.dist))
 ruleSPEqualityGaussianRGMP(msg_1::Message{F1}, msg_2::Nothing, msg_3::Message{F2}) where {F1<:Gaussian, F2<:Function}= Message(prod!(msg_1.dist, msg_3.dist))
 ruleSPEqualityGaussianRGMP(msg_1::Nothing, msg_2::Message{F1}, msg_3::Message{F2}) where {F1<:Gaussian, F2<:Function} = Message(prod!(msg_2.dist, msg_3.dist))
-ruleSPEqualityGaussianGCV(msg_1::Message{F1},msg_2::Message{F2},msg_3::Nothing) where {F1<:Gaussian, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_1.dist,msg_2.dist))
-ruleSPEqualityGaussianGCV(msg_1::Message{F2},msg_2::Message{F1},msg_3::Nothing) where {F1<:Gaussian, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_1.dist,msg_2.dist))
-ruleSPEqualityGaussianGCV(msg_1::Message{F1},msg_2::Nothing,msg_3::Message{F2}) where {F1<:Gaussian, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_1.dist,msg_3.dist))
-ruleSPEqualityGaussianGCV(msg_1::Message{F2},msg_2::Nothing,msg_3::Message{F1}) where {F1<:Gaussian, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_1.dist,msg_3.dist))
-ruleSPEqualityGaussianGCV(msg_1::Nothing,msg_2::Message{F1},msg_3::Message{F2}) where {F1<:Gaussian, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_2.dist,msg_3.dist))
-ruleSPEqualityGaussianGCV(msg_1::Nothing,msg_2::Message{F2},msg_3::Message{F1}) where {F1<:Gaussian, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_2.dist,msg_3.dist))
-
-
-ruleSPEqualityGaussianGCV(msg_1::Message{F1},msg_2::Message{F2},msg_3::Nothing) where {F1<:ExponentialLinearQuadratic, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_1.dist,msg_2.dist))
-ruleSPEqualityGaussianGCV(msg_1::Message{F1},msg_2::Nothing,msg_3::Message{F2}) where {F1<:ExponentialLinearQuadratic, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_1.dist,msg_3.dist))
-ruleSPEqualityGaussianGCV(msg_1::Nothing,msg_2::Message{F2},msg_3::Message{F1}) where {F1<:ExponentialLinearQuadratic, F2<:ExponentialLinearQuadratic} = Message(prod!(msg_2.dist,msg_3.dist))
--- src/update_rules/equality.jl
+++ src/update_rules/equality.jl
@@ -79,22 +79,3 @@ function isApplicable(::Type{SPEqualityGaussianRGMP}, input_types::Vector{Type})
 
     return (nothing_inputs == 1) && (function_inputs == 1) && (gaussian_inputs == 1)
 end
-
-mutable struct SPEqualityGaussianGCV <: SumProductRule{Equality} end
-outboundType(::Type{SPEqualityGaussianGCV}) = Message{GaussianMeanVariance}
-function isApplicable(::Type{SPEqualityGaussianGCV}, input_types::Vector{Type})
-    nothing_inputs = 0
-    gaussian_inputs = 0
-    exp_lin_quad_inputs = 0
-    for input_type in input_types
-        if input_type == Nothing
-            nothing_inputs += 1
-        elseif matches(input_type, Message{Gaussian})
-            gaussian_inputs += 1
-        elseif matches(input_type, Message{ExponentialLinearQuadratic})
-            exp_lin_quad_inputs += 1
-        end
-    end
-
-    return (nothing_inputs == 1) && (gaussian_inputs == 1) && (exp_lin_quad_inputs == 1)
-end
